0d91b943198e7511ff0f7d15c7f1028e47a55a6a
Brad Alexander <brad.alexander913@gmail.com>
2019-11-07 00:53:38 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -48,4 +48,9 @@ class InteractiveRecord
     column_names.compact
   end
 
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    DB[:conn].execute(sql)
+  end
+
 end
