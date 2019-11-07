e74879b8d37bc058b5500e3b5fc6a651cec6256c
Brad Alexander <brad.alexander913@gmail.com>
2019-11-07 00:48:58 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -16,9 +16,9 @@ class InteractiveRecord
   end
 
   def table_name_for_insert
-    self.class.table_name 
+    self.class.table_name
   end
-  
+
   def values_for_insert
    values = []
    self.class.column_names.each do |col_name|
